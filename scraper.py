691a8e271572afb8851e6071b30af7ae51b84bb1
Anushk Farkiya <45423129+Anushk97@users.noreply.github.com>
2023-08-03 10:41:24 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -75,104 +75,106 @@ def clean_and_split(value):
 start = 0
 end = batch_size
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-    @st.cache_data
-    def get_driver():
-        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-features=NetworkService")
+options.add_argument("--window-size=1920x1080")
+options.add_argument("--disable-features=VizDisplayCompositor")
+#options.add_argument('--headless')
 
-    options = Options()
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
+#driver = get_driver()
+driver = webdriver.Chrome(options=options, service_log_path='selenium.log')
+#driver.get('https://thinkhazard.org/en/')
+#st.code(driver.page_source)
 
-    driver = get_driver()
-    driver.get('https://thinkhazard.org/en/')
-    st.code(driver.page_source)
-    
-    while start < end:
-        data_cop = dataset[start:end]
-        data_to_merge = data_cop[['Country','Region (HL)','Region (Granular)']]
-        data_to_merge['Region (Granular'] = data_to_merge['Region (Granular)'].astype(str)
-        granular = data_cop['Region (Granular)']
-    
-        #driver = webdriver.Chrome(options=chrome_options)
-        #driver = webdriver.Chrome(ChromeDriverManager().install())
-        #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=option)
-        #driver = webdriver.Chrome()
-        #driver.set_window_size(window_size_x, window_size_y)
-    
-        res = []
-        val = []
-        blocked = []
-        st.write(granular)
-        for i in granular:
-            try:
-                #driver.get('https://thinkhazard.org/en/')
-                #assert "Python" in driver.title
-                driver.find_element(By.XPATH,'//*[@id="myModal"]/div/div/div[2]/button[2]').click()
-                driver.find_element(By.XPATH,'/html/body/div[2]/div/form/span[2]/input[2]').send_keys(i)
-                driver.implicitly_wait(60)
-                driver.find_element(By.XPATH, '//*[@id="search"]/span[2]/div/div/div[1]').click()
-                driver.implicitly_wait(60)
-                URL = driver.current_url
-                resp = requests.get(URL)
-                soup = BeautifulSoup(resp.text, features='lxml')
-    
-                for j in soup.find_all('h2', {'class': 'page-header'}):
-                    val.append(i)
-                    res.append(j.get_text())
-    
-            except:
-                print(i, 'blocked')
-                blocked.append(i)
-    
-            #pbar.update(1)
-            #my_bar.progress(x + 1, text='in progress')
-            start += 1
-    
-        processed = [clean_and_split(value) for value in res]
-    
-        df = pd.DataFrame(processed, columns=['Col', 'level'])
-        df['Country'] = data_cop['Country']
-        df['Region HL'] = data_cop['Region (HL)']
-        df['Region (Granular)'] = val
-        block = pd.DataFrame(blocked, columns=['blocked'])
-    
-    
-        table = pd.pivot_table(df, index=['Region (Granular)'], columns='Col', values='level', aggfunc=lambda x: ' '.join(x), sort = False)
-        #table['Region (Granular)'] = table['Region (Granular)'].astype(str)
-        #final = pd.merge(table, data_to_merge, on =
-        final = pd.concat([table, data_to_merge.set_index('Region (Granular)')], axis=1).reset_index()
-        #final = final[['Country', 'Region (HL)', 'Region (Granular)', 'River flood', 'Urban flood', 'Earthquake', 'Landslide', 'Wildfire', 'Water scarcity', 'Cyclone', 'Extreme heat', 'Coastal flood', 'Tsunami', 'Volcano']]
-    
-        output_data = output_data._append(final)
-        blocked_data = blocked_data._append(block)
-    
-        end += 10
-        if end > len(dataset):
-            break
-        else:
-            continue
-    
-    st.write("loop ended")
-    def convert_df(df):
-       return df.to_csv(index=False).encode('utf-8')
-    
-    csv = convert_df(output_data)
-    
-    st.download_button(
-       "Press to Download",
-       csv,
-       "file.csv",
-       "text/csv",
-       key='download-csv'
-    )
-    
-    sys.exit()
+while start < end:
+    data_cop = dataset[start:end]
+    data_to_merge = data_cop[['Country','Region (HL)','Region (Granular)']]
+    data_to_merge['Region (Granular'] = data_to_merge['Region (Granular)'].astype(str)
+    granular = data_cop['Region (Granular)']
+
+    #driver = webdriver.Chrome(options=chrome_options)
+    #driver = webdriver.Chrome(ChromeDriverManager().install())
+    #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=option)
+    #driver = webdriver.Chrome()
+    #driver.set_window_size(window_size_x, window_size_y)
+
+    res = []
+    val = []
+    blocked = []
+    st.write(granular)
+    for i in granular:
+        try:
+            driver.get('https://thinkhazard.org/en/')
+            #assert "Python" in driver.title
+            driver.find_element(By.XPATH,'//*[@id="myModal"]/div/div/div[2]/button[2]').click()
+            driver.find_element(By.XPATH,'/html/body/div[2]/div/form/span[2]/input[2]').send_keys(i)
+            driver.implicitly_wait(60)
+            driver.find_element(By.XPATH, '//*[@id="search"]/span[2]/div/div/div[1]').click()
+            driver.implicitly_wait(60)
+            URL = driver.current_url
+            resp = requests.get(URL)
+            soup = BeautifulSoup(resp.text, features='lxml')
+
+            for j in soup.find_all('h2', {'class': 'page-header'}):
+                val.append(i)
+                res.append(j.get_text())
+
+        except:
+            print(i, 'blocked')
+            blocked.append(i)
+
+        #pbar.update(1)
+        #my_bar.progress(x + 1, text='in progress')
+        start += 1
+
+    processed = [clean_and_split(value) for value in res]
+
+    df = pd.DataFrame(processed, columns=['Col', 'level'])
+    df['Country'] = data_cop['Country']
+    df['Region HL'] = data_cop['Region (HL)']
+    df['Region (Granular)'] = val
+    block = pd.DataFrame(blocked, columns=['blocked'])
+
+
+    table = pd.pivot_table(df, index=['Region (Granular)'], columns='Col', values='level', aggfunc=lambda x: ' '.join(x), sort = False)
+    #table['Region (Granular)'] = table['Region (Granular)'].astype(str)
+    #final = pd.merge(table, data_to_merge, on =
+    final = pd.concat([table, data_to_merge.set_index('Region (Granular)')], axis=1).reset_index()
+    #final = final[['Country', 'Region (HL)', 'Region (Granular)', 'River flood', 'Urban flood', 'Earthquake', 'Landslide', 'Wildfire', 'Water scarcity', 'Cyclone', 'Extreme heat', 'Coastal flood', 'Tsunami', 'Volcano']]
+
+    output_data = output_data._append(final)
+    blocked_data = blocked_data._append(block)
+
+    end += 10
+    if end > len(dataset):
+        break
+    else:
+        continue
+
+st.write("loop ended")
+def convert_df(df):
+   return df.to_csv(index=False).encode('utf-8')
+
+csv = convert_df(output_data)
+
+st.download_button(
+   "Press to Download",
+   csv,
+   "file.csv",
+   "text/csv",
+   key='download-csv'
+)
+
+sys.exit()
 
 #SAVE
 #print(output_data)
