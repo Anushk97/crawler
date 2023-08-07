bb7f65c4f44b5fb43251dabe3e9a456d8de91eb2
Anushk Farkiya <45423129+Anushk97@users.noreply.github.com>
2023-08-07 18:15:40 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Project Crawler')
+st.title('Project Crawler!')
 
 with st.form("my-form", clear_on_submit=True):
     uploaded_file = st.file_uploader("upload file")
@@ -56,7 +56,7 @@ blocked_data = pd.DataFrame()
 
 total_iterations = len(dataset)
 #pbar = tqdm(total=total_iterations, desc='Progress', unit='iteration')
-my_bar = st.progress(0, text="in progress")
+#my_bar = st.progress(0, text="in progress")
 
 #Step 3: Crawler
 #service = Service(r"D:\Users\anushk.farkiya\Downloads\chromedriver_win32")
@@ -92,75 +92,75 @@ driver = webdriver.Chrome(service=service, options=options)
 #driver = webdriver.Chrome(options=options)
 #driver.get('https://thinkhazard.org/en/')
 #st.code(driver.page_source)
+batch = 1
 total_batches = int(len(dataset)/batch_size)
 st.write('total batches', total_batches) 
-for k in range(1, total_batches + 1):
-    while start < end:
-        data_cop = dataset[start:end]
-        data_to_merge = data_cop[['Country','Region (HL)','Region (Granular)']]
-        data_to_merge['Region (Granular'] = data_to_merge['Region (Granular)'].astype(str)
-        granular = data_cop['Region (Granular)']
+
+while start < end:
+    data_cop = dataset[start:end]
+    data_to_merge = data_cop[['Country','Region (HL)','Region (Granular)']]
+    #data_to_merge['Region (Granular'] = data_to_merge['Region (Granular)'].astype(str)
+    granular = data_cop['Region (Granular)']
+
+    #driver = webdriver.Chrome(options=chrome_options)
+    #driver = webdriver.Chrome(ChromeDriverManager().install())
+    #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=option)
+    #driver = webdriver.Chrome()
+    #driver.set_window_size(window_size_x, window_size_y)
+
+    res = []
+    val = []
+    blocked = []
+    st.write('batch:',batch)
+    for i in granular:
+        try:
+            driver.get('https://thinkhazard.org/en/')
+            #assert "Python" in driver.title
+            driver.find_element(By.XPATH,'//*[@id="myModal"]/div/div/div[2]/button[2]').click()
+            driver.find_element(By.XPATH,'/html/body/div[2]/div/form/span[2]/input[2]').send_keys(i)
+            driver.implicitly_wait(60)
+            driver.find_element(By.XPATH, '//*[@id="search"]/span[2]/div/div/div[1]').click()
+            driver.implicitly_wait(60)
+            URL = driver.current_url
+            resp = requests.get(URL)
+            soup = BeautifulSoup(resp.text, features='lxml')
+
+            for j in soup.find_all('h2', {'class': 'page-header'}):
+                val.append(i)
+                res.append(j.get_text())
+
+        except:
+            print(i, 'blocked')
+            blocked.append(i)
+
+        #pbar.update(1)
+        start += 1
+
+    processed = [clean_and_split(value) for value in res]
+
+    df = pd.DataFrame(processed, columns=['Col', 'level'])
+    df['Country'] = data_cop['Country']
+    df['Region HL'] = data_cop['Region (HL)']
+    df['Region (Granular)'] = val
+    block = pd.DataFrame(blocked, columns=['blocked'])
+
+
+    table = pd.pivot_table(df, index=['Region (Granular)'], columns='Col', values='level', aggfunc=lambda x: ' '.join(x), sort = False)
+    #table['Region (Granular)'] = table['Region (Granular)'].astype(str)
+    #final = pd.merge(table, data_to_merge, on =
+    #final = pd.concat([table, data_to_merge.set_index('Region (Granular)')], axis=1).reset_index()
+    final = pd.merge(data_to_merge, table, on='Region (Granular)')
+    final = final[['Country', 'Region (HL)', 'Region (Granular)', 'River flood', 'Coastal flood', 'Wildfire', 'Urban flood', 'Landslide', 'Tsunami', 'Water scarcity', 'Extreme heat', 'Cyclone', 'Volcano', 'Earthquake']]
     
-        #driver = webdriver.Chrome(options=chrome_options)
-        #driver = webdriver.Chrome(ChromeDriverManager().install())
-        #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=option)
-        #driver = webdriver.Chrome()
-        #driver.set_window_size(window_size_x, window_size_y)
-    
-        res = []
-        val = []
-        blocked = []
-        st.write('batch:',k)
-        for i in granular:
-            try:
-                driver.get('https://thinkhazard.org/en/')
-                #assert "Python" in driver.title
-                driver.find_element(By.XPATH,'//*[@id="myModal"]/div/div/div[2]/button[2]').click()
-                driver.find_element(By.XPATH,'/html/body/div[2]/div/form/span[2]/input[2]').send_keys(i)
-                driver.implicitly_wait(60)
-                driver.find_element(By.XPATH, '//*[@id="search"]/span[2]/div/div/div[1]').click()
-                driver.implicitly_wait(60)
-                URL = driver.current_url
-                resp = requests.get(URL)
-                soup = BeautifulSoup(resp.text, features='lxml')
-    
-                for j in soup.find_all('h2', {'class': 'page-header'}):
-                    val.append(i)
-                    res.append(j.get_text())
-    
-            except:
-                print(i, 'blocked')
-                blocked.append(i)
-    
-            #pbar.update(1)
-            start += 1
-    
-        processed = [clean_and_split(value) for value in res]
-    
-        df = pd.DataFrame(processed, columns=['Col', 'level'])
-        df['Country'] = data_cop['Country']
-        df['Region HL'] = data_cop['Region (HL)']
-        df['Region (Granular)'] = val
-        block = pd.DataFrame(blocked, columns=['blocked'])
-    
-    
-        table = pd.pivot_table(df, index=['Region (Granular)'], columns='Col', values='level', aggfunc=lambda x: ' '.join(x), sort = False)
-        #table['Region (Granular)'] = table['Region (Granular)'].astype(str)
-        #final = pd.merge(table, data_to_merge, on =
-        final = pd.concat([table, data_to_merge.set_index('Region (Granular)')], axis=1).reset_index()
-        #final = pd.merge(data_to_merge, table, on='Region (Granular)')
-        #final = final[['Country', 'Region (HL)', 'Region (Granular)', 'River flood', 'Coastal flood', 'Wildfire', 'Urban flood', 'Landslide', 'Tsunami', 'Water scarcity', 'Extreme heat', 'Cyclone', 'Volcano', 'Earthquake']]
-        
-        output_data = output_data._append(final)
-        blocked_data = blocked_data._append(block)
-    
-        end += batch_size
-        my_bar.progress(k + 1, text='in progress')
-        k += 1
-        if end > len(dataset):
-            break
-        else:
-            continue
+    output_data = output_data._append(final)
+    blocked_data = blocked_data._append(block)
+
+    end += batch_size
+    batch += 1
+    if end > len(dataset):
+        break
+    else:
+        continue
 
 st.write("loop ended")
 def convert_df(df):
@@ -172,14 +172,6 @@ csv_blocked = convert_df(blocked_data)
 st.download_button(
    "Press to Download output",
    csv,
-   "file.csv",
-   "text/csv",
-   key='download-csv'
-)
-
-st.download_button(
-   "Press to Download blocked values",
-   csv_blocked,
    "file.csv",
    "text/csv",
    key='download-csv'
