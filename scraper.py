0b011d67f198bd37d39e12077629b0b26d94f7c6
Anushk Farkiya <45423129+Anushk97@users.noreply.github.com>
2023-08-08 09:23:12 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -23,7 +23,7 @@ st.write(dataset)
 #Change this values accordingly
 window_size_x = 800 #Enable only when to see the crawler
 window_size_y = 800
-batch_size = 100
+batch_size = 200
 #file_path_source = r'\Users\anushk.farkiya\Downloads\webscraping output - climate data.xlsx'
 #path_to_save_output = r"\Users\anushk.farkiya\PycharmProjects\pythonProject\automate\final_output_3.xlsx"
 #path_to_blocked_values = r'\Users\anushk.farkiya\PycharmProjects\pythonProject\automate\blocked_batch_3.xlsx'
