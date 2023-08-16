d474a0ac9affd7f1200158c14f3ec641acaa85e6
Anushk Farkiya <45423129+Anushk97@users.noreply.github.com>
2023-08-16 17:47:58 +0800

    Update optimize.py

--- optimize.py
+++ optimize.py
@@ -44,7 +44,7 @@ data = pd.read_excel(uploaded_file)
 #data = data.drop(labels=1, axis = 0)
 dataset = data[:40]
 #print(data)
-data = data[['Country', 'Region (HL)', 'Region (Granular)', 'River flood', 'Urban flood', 'Earthquake', 'Landslide', 'Wildfires', 'Water scarcity', 'Cyclone', 'Extreme heat', 'Coastal flood', 'Tsunami', 'Volcano']]
+#data = data[['Country', 'Region (HL)', 'Region (Granular)', 'River flood', 'Urban flood', 'Earthquake', 'Landslide', 'Wildfires', 'Water scarcity', 'Cyclone', 'Extreme heat', 'Coastal flood', 'Tsunami', 'Volcano']]
 #print(data)
 
 output_data = pd.DataFrame()
@@ -149,6 +149,9 @@ while start < end:
         for _, row in final.iterrows():
             csv_writer.writerow(row)
 
+    if batch <= total_batches:
+        batch += 1
+    
     if end < len(data):
         end += batch_size
 
