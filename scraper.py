44773c150a8a6a5dc2939aa40df0363d4c2deadf
Anushk Farkiya <45423129+Anushk97@users.noreply.github.com>
2023-08-16 15:25:46 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,7 +17,7 @@ dataframe = pd.read_excel(uploaded_file)
 #data = dataframe.drop(labels=0, axis = 0)
 #data.columns = data.iloc[0]
 #data = data.drop(labels=1, axis = 0)
-dataset = data[:30]
+dataset = dataframe[:30]
 
 st.write(dataset)
 
