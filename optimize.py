260c363533663b6bbac163c04f0344309201197c
Anushk Farkiya <45423129+Anushk97@users.noreply.github.com>
2023-08-17 18:39:20 +0800

    Update optimize.py

--- optimize.py
+++ optimize.py
@@ -93,6 +93,7 @@ while start < end:
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
 
     # driver = get_driver()
     service = Service()
