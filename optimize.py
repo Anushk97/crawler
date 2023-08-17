297f4f9fc297ceb98ca834ff7d77ee19072eb19a
Anushk Farkiya <45423129+Anushk97@users.noreply.github.com>
2023-08-17 18:26:35 +0800

    Update optimize.py

--- optimize.py
+++ optimize.py
@@ -111,9 +111,9 @@ while start < end:
             driver.get('https://thinkhazard.org/en/')
             driver.find_element(By.XPATH,'//*[@id="myModal"]/div/div/div[2]/button[2]').click()
             driver.find_element(By.XPATH,'/html/body/div[2]/div/form/span[2]/input[2]').send_keys(i)
-            driver.implicitly_wait(20)
+            driver.implicitly_wait(60)
             driver.find_element(By.XPATH, '//*[@id="search"]/span[2]/div/div/div[1]').click()
-            driver.implicitly_wait(10)
+            driver.implicitly_wait(60)
             URL = driver.current_url
             resp = requests.get(URL)
             soup = BeautifulSoup(resp.text, features='html.parser')
