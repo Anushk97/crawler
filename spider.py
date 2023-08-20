8bf3f1c24315e0d338d9cefa1fc487ddd490e5cc
Anushk Farkiya <45423129+Anushk97@users.noreply.github.com>
2023-08-20 10:01:34 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -104,7 +104,7 @@ def main():
     run_spider_button = st.button("Run Scrapy Spider")
     if run_spider_button:
         # Run the Scrapy spider using subprocess
-        subprocess.run(["spider"])
+        subprocess.run(["scrapy", "my_spider_2", "spider.py"])
 
     # Display the download button for the CSV file
     download_button = st.button("Download CSV")
